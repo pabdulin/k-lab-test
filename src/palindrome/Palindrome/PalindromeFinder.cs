8554446159c9f68648561c94e0771f72059e55a9
Petr Abdulin <pabdulin@gmail.com>
2017-05-26 00:20:03 +0700

    Palindrome initial attempt working.

---
+++ src/palindrome/Palindrome/PalindromeFinder.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Palindrome
+{
+    internal struct finder_match
+    {
+        public finder_match(int position, int length)
+        {
+            Position = position;
+            Length = length;
+        }
+
+        public int Position;
+        public int Length;
+    }
+
+    public class PalindromeFinder
+    {
+        public string SearchPalindrome(string text)
+        {
+            if (text == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (text == string.Empty)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+            // leftmost character is always good result
+            finder_match bestMatch = new finder_match(0, 1);
+
+            for (int startIndex = 0; startIndex < text.Length; startIndex += 1)
+            {
+                int leftPos = startIndex;
+                int rightPos = text.Length;
+                while (leftPos < rightPos - 1) // -1 for ignoring substrings lenght of 1
+                {
+                    var test = text.Substring(leftPos, rightPos - leftPos);
+                    if (IsPalindrome(test))
+                    {
+                        if (bestMatch.Length < test.Length)
+                        {
+                            bestMatch = new finder_match(leftPos, test.Length);
+                        }
+                    }
+                    rightPos -= 1;
+                }
+            }
+
+            string result = text.Substring(0, 1); 
+            if(bestMatch.Length > 0)
+            {
+                result = text.Substring(bestMatch.Position, bestMatch.Length);
+            }
+
+            return result;
+        }
+
+        public static bool IsPalindrome(string text)
+        {
+            var result = true;
+
+            for (int i = 0; i < text.Length / 2; i += 1)
+            {
+                if (text[i] != text[text.Length - 1 - i])
+                {
+                    result = false;
+                    break;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ src/palindrome/PalindromeTest/PalindromeFinderTest.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Palindrome;
+
+namespace PalindromeTest
+{
+    [TestClass]
+    public class PalindromeFinderTest
+    {
+        private PalindromeFinder _target = new PalindromeFinder();
+
+        private string[] _testPalindromes = new string[]
+        {
+            "c", "cbabc", "aczzca", "arozaupalanalapuazora"
+        };
+
+        private string[] _testNotPalindromes = new string[]
+        {
+           "ab", "abc", "arozaupaalanalapuazora"
+        };
+
+
+        [TestMethod]
+        public void ShouldPassSampleTest()
+        {
+            var input = "sometextarozaupalanalapuazorasomeanothertext";
+            var expected = "arozaupalanalapuazora";
+            var result = _target.SearchPalindrome(input);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ShouldReturnLeftmostIfSameLength()
+        {
+            var input = "zraabbds";
+            var expected = "aa";
+            var result = _target.SearchPalindrome(input);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ShouldReturnMaxLength()
+        {
+            var input = "zrcaacbabcds";
+            var expected = "cbabc";
+            var result = _target.SearchPalindrome(input);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ShouldWorkOnPolindromeOnly()
+        {
+            foreach (var input in _testPalindromes)
+            {
+                var expected = input;
+                var result = _target.SearchPalindrome(input);
+                Assert.AreEqual(expected, result);
+            }
+        }
+
+        [TestMethod]
+        public void ShouldWorkOnSingleDigitLeftmost()
+        {
+            var input = "cba";
+            var expected = "c";
+            var result = _target.SearchPalindrome(input);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ShouldDetectPalindrome()
+        {
+            var expected = true;
+            foreach (var input in _testPalindromes)
+            {
+                var result = PalindromeFinder.IsPalindrome(input);
+                Assert.AreEqual(expected, result);
+            }
+        }
+
+        [TestMethod]
+        public void ShouldDetectNotPalindrome()
+        {
+            var expected = false;
+            foreach (var input in _testNotPalindromes)
+            {
+                var result = PalindromeFinder.IsPalindrome(input);
+                Assert.AreEqual(expected, result);
+            }
+        }
+    }
+}
