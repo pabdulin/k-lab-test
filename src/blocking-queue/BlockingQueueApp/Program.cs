89109939240040a8119ec8f1b638f462279b6659
Petr Abdulin <pabdulin@gmail.com>
2017-05-27 16:36:25 +0700

    Add test app.

---
+++ src/blocking-queue/BlockingQueueApp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlockingQueueApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Trace.Listeners.Add(new ConsoleTraceListener());
+
+            if (Debugger.IsAttached)
+            {
+                Console.WriteLine("Press any key to exit..");
+                Console.ReadKey();
+            }
+        }
+    }
+}
