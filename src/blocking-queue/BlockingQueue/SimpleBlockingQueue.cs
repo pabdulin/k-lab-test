6873882de9cc911cfd826df28ae11293db9bcede
Petr Abdulin <pabdulin@gmail.com>
2017-05-27 16:10:24 +0700

    First try using AutoResetEvent.

---
+++ src/blocking-queue/BlockingQueue/SimpleBlockingQueue.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+
+namespace BlockingQueue
+{
+    public class SimpleBlockingQueue : IBlockingQueue
+    {
+        private bool _isDisabled = false;
+        private Queue<object> _store = new Queue<object>();
+        private readonly AutoResetEvent _empty = new AutoResetEvent(false);
+
+        /// <summary>
+        /// Put кладет новый элемент в очередь.
+        /// </summary>
+        public void Put(object o)
+        {
+            _store.Enqueue(o);
+            _empty.Set();
+        }
+
+        /// <summary>
+        /// Get должен блокировать выполнение потока если в очереди нет элементов до тех пор, 
+        /// пока не появится элемент, который можно вернуть.
+        /// </summary>
+        public object Get()
+        {
+            if (!_isDisabled)
+            {
+                _empty.WaitOne();
+            }
+
+            if (_store.Count > 0)
+            {
+                var result = _store.Dequeue();
+                return result;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Disable должен освобождать все ждущие потоки. 
+        /// При этом Get должен возвращать null (иликидать исключение - как больше нравится). 
+        /// Все последующие вызовы к Get должны просто возвращать null.
+        /// </summary>
+        public void Disable()
+        {
+            _isDisabled = true;
+            _empty.Set();
+        }
+    }
+}
---
+++ src/blocking-queue/BlockingQueueTest/SimpleBlockingQueueTest.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BlockingQueue;
+using System.Threading;
+
+namespace BlockingQueueTest
+{
+    [TestClass]
+    public class SimpleBlockingQueueTest
+    {
+        private static readonly SimpleBlockingQueue _static1 = new SimpleBlockingQueue();
+
+        [TestMethod]
+        [Timeout(1000)]
+        public void ShouldPutAndGet()
+        {
+            SimpleBlockingQueue target = new SimpleBlockingQueue();
+            int testValue = 1;
+            target.Put(testValue);
+            var testValueBack = target.Get();
+            Assert.AreEqual(testValue, testValueBack);
+        }
+
+        [TestMethod]
+        [Timeout(1000)]
+        public void ShouldReturnNullAfterDisabling()
+        {
+            SimpleBlockingQueue target = new SimpleBlockingQueue();
+            //int testValue = 1;
+            //target.Put(testValue);
+            target.Disable();
+            var testValueBack = target.Get();
+            Assert.AreEqual(null, testValueBack);
+        }
+
+        //[TestMethod]
+        //[Timeout(100)]
+        //public void ShouldBlockOnEmptyQueue()
+        //{
+        //    SimpleBlockingQueue target = new SimpleBlockingQueue();
+        //    //int testValue = 1;
+        //    //target.Put(testValue);
+        //    //target.Disable();
+        //    var testValueBack = target.Get();
+        //}
+
+        [TestMethod]
+        [Timeout(1000)]
+        public void Multithread()
+        {
+            Thread[] threads = new Thread[2];
+            object _res1 = null;
+            object _res2 = null;
+
+            threads[0] = new Thread(() =>
+            {
+                _res1 = _static1.Get();
+                _res2 = _static1.Get();
+            });
+
+            threads[1] = new Thread(() =>
+            {
+                _static1.Put(1);
+                _static1.Put(2);
+            });
+
+            for (int i = 0; i < threads.Length; i += 1)
+            {
+                threads[i].Start();
+            }
+
+            for (int i = 0; i < threads.Length; i += 1)
+            {
+                threads[i].Join();
+            }
+
+            Assert.AreEqual(1, _res1);
+            Assert.AreEqual(2, _res2);
+        }
+    }
+}
